db7a7dd2912436b80265ef371d38720b951ffd0a
zhangw <935279748@qq.com>
2020-03-10 15:00:08 +0800

    nit component

--- src/NAMap.ts
+++ src/NAMap.ts
@@ -9,9 +9,11 @@ type Info = RenderInfo & { type: React.ComponentType };
  */
 export default class NAMap {
   private instance: Map<React.ComponentType, number[]>;
+  private domMap: Map<number, number>;
 
   constructor() {
     this.instance = new Map();
+    this.domMap = new Map();
   }
 
   /**
@@ -19,7 +21,7 @@ export default class NAMap {
    * @param key
    * @param value
    */
-  setSingle(key: React.ComponentType, value: number) {
+  private setSingle(key: React.ComponentType, value: number, dom: number) {
     const current = this.instance.get(key);
     if (current) {
       if (current[0] && current[0] > value) {
@@ -30,18 +32,16 @@ export default class NAMap {
     } else {
       this.instance.set(key, [value]);
     }
+
+    this.domMap.set(value, dom);
   }
 
-  set(key: React.ComponentType, value: number[]) {
-    this.instance.set(key, value);
-  }
-
-  push(items: Info | Info[]) {
+  push(items: (Info & { dom: number }) | (Info & { dom: number })[]) {
     if (!(items instanceof Array)) {
-      this.setSingle(items.type, items.i);
+      this.setSingle(items.type, items.i, items.dom);
     } else {
       items.forEach(item => {
-        this.setSingle(item.type, item.i);
+        this.setSingle(item.type, item.i, item.dom);
       });
     }
   }
@@ -55,6 +55,8 @@ export default class NAMap {
         return true;
       }
     }
+
+    this.domMap.delete(i);
     return false;
   }
 
@@ -70,5 +72,9 @@ export default class NAMap {
     } else {
       return undefined;
     }
+  }
+
+  getDom(key: number) {
+    return this.domMap.get(key);
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { isUndef } from './util';
 
 type HeaderFooterProps = {
   onHeightChange: (height: number) => void;
+  style: React.CSSProperties;
 };
 
 export type CellData<T> = {
@@ -30,7 +31,17 @@ export type RecyclerListProps<T> = {
   renderAccuary?: number;
 };
 
-type Layout = { height: number; top: number; type: React.ComponentType };
+type Layout = {
+  height: number;
+  top: number;
+  type: React.ComponentType<{
+    height: number;
+    width: number;
+    top: number;
+    data: any;
+    index: number;
+  }>;
+};
 
 export type RenderInfo = { i: number; dom: number };
 
@@ -42,10 +53,23 @@ type State<T> = {
   current: RenderInfo[];
   renderCurrent: number[];
   renderCurrentMap: NAMap;
+  contentHeight: number;
 };
 
 const scrollStyle: React.CSSProperties = {
   WebkitOverflowScrolling: 'touch'
+};
+
+const headerStyle: React.CSSProperties = {
+  position: 'absolute',
+  top: 0,
+  left: 0
+};
+
+const footerStyle: React.CSSProperties = {
+  position: 'absolute',
+  bottom: 0,
+  left: 0
 };
 
 class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
@@ -59,7 +83,7 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
       totalHeight += height;
     }
 
-    return layouts;
+    return { layouts, contentHeight: totalHeight };
   }
 
   static getDerivedStateFromProps<T>(
@@ -73,7 +97,7 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
       return null;
     } else {
       return {
-        layouts: RecyclerList.computedRenderCellLayouts(props),
+        ...RecyclerList.computedRenderCellLayouts(props),
         cellData: props.cellData
       };
     }
@@ -88,7 +112,8 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
     layouts: [],
     current: [],
     renderCurrent: [],
-    renderCurrentMap: new NAMap()
+    renderCurrentMap: new NAMap(),
+    contentHeight: 0
   };
 
   lastScrollTop: number = 0;
@@ -142,20 +167,14 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
     return {
       current: newCurrent,
       renderCurrent: newRenderCurrent,
-      renderCurrentMap
+      renderCurrentMap,
+      shouldSetState: true
     };
   }
 
   private getRenderList(scrollTop: number) {
-    const {
-      current,
-      renderCurrent,
-      headerHeight,
-      layouts,
-      renderCurrentMap
-    } = this.state;
+    const { current, headerHeight, layouts, renderCurrentMap } = this.state;
     const { height, renderAccuary = 3 } = this.props;
-    const itemCount = layouts.length;
     const lastScrollTop = this.lastScrollTop;
     this.lastScrollTop = scrollTop;
 
@@ -167,44 +186,118 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
     // 需要render真实item的区域
     const bottomOffset = ((renderAccuary - 1) / 2 + 1) * height;
     const topOffset = (height * (renderAccuary - 1)) / 2;
-    const start = Math.max(0, scrollTop - topOffset);
-    const end = scrollTop + bottomOffset;
+    const start = Math.max(0, scrollTop - topOffset) - headerHeight;
+    const end = scrollTop + bottomOffset - headerHeight;
+
+    let shouldSetState = false;
 
     if (current.length === 0) {
       return this.getRenderListFromEmpty(start, end);
     } else if (isScrollDown) {
       while (true) {
         const last = current[current.length - 1];
+        if (last.i === len - 1) break;
         const lastLayout = layouts[last.i];
         const shouldAddNewItem = lastLayout.top + lastLayout.height >= end;
         if (shouldAddNewItem) {
-          const nextItem = layouts[last.i + 1];
+          shouldSetState = true;
+          const ii = last.i + 1;
+          const nextItem = layouts[ii];
           const oldItem = renderCurrentMap.getFirst(nextItem.type);
-          if (oldItem) {
+          if (isUndef(typeof oldItem)) {
             const oldItemLayout = layouts[oldItem];
             const shouldReuseOldItem =
               oldItemLayout.top + oldItemLayout.height < start;
             if (shouldReuseOldItem) {
+              const dom = renderCurrentMap.getDom(oldItem);
+              // 移除旧的item
               renderCurrentMap.remove(oldItemLayout.type, oldItem);
+              const oldIndex = current.findIndex(item => item.i === oldItem);
+              current.splice(oldIndex, 1);
+
+              // 复用旧的 item 并且插入新的item
+              current.push({ i: ii, dom });
+            } else {
+              // 没有旧的模块可以复用，直接插入新的模块
+              current.push({ i: ii, dom: current.length });
             }
+          } else {
+            // 没有旧的模块可以复用，直接插入新的模块
+            current.push({ i: ii, dom: current.length });
           }
         } else {
+          // 不需要向末尾增加模块了, 跳出循环
           break;
         }
       }
     } else {
+      while (true) {
+        const first = current[0];
+        if (first.i === 0) break;
+        const firstLayout = layouts[first.i];
+        const shouldAddNewItem = firstLayout.top > start;
+        if (shouldAddNewItem) {
+          shouldSetState = true;
+          const ii = first.i - 1;
+          const nextItem = layouts[ii];
+          const oldItem = renderCurrentMap.getLast(nextItem.type);
+          if (isUndef(typeof oldItem)) {
+            const oldItemLayout = layouts[oldItem];
+            const shouldReuseOldItem = oldItemLayout.top > end;
+            if (shouldReuseOldItem) {
+              const dom = renderCurrentMap.getDom(oldItem);
+              // 移除旧的item
+              renderCurrentMap.remove(oldItemLayout.type, oldItem);
+              const oldIndex = current.findIndex(item => item.i === oldItem);
+              current.splice(oldIndex, 1);
+              // 复用旧的 item 并且插入新的item
+              current.unshift({ i: ii, dom });
+            } else {
+              // 没有旧的模块可以复用，直接插入新的模块
+              current.unshift({ i: ii, dom: current.length });
+            }
+          } else {
+            // 没有旧的模块可以复用，直接插入新的模块
+            current.unshift({ i: ii, dom: current.length });
+          }
+        } else {
+          // 不需要向末尾增加模块了, 跳出循环
+          break;
+        }
+      }
     }
+
+    const newRenderCurrent = current.sort((a, b) => a.i - b.i).map(i => i.i);
+
+    return {
+      current,
+      renderCurrent: newRenderCurrent,
+      renderCurrentMap,
+      shouldSetState
+    };
   }
 
   handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     const container = this.container.current;
     if (container) {
       const { scrollTop } = container;
+      const { shouldSetState, ...oterState } = this.getRenderList(scrollTop);
+      if (shouldSetState) {
+        this.setState(oterState);
+      }
     }
   };
 
   render() {
     const { height, width, style, className, Header, Footer } = this.props;
+    const {
+      renderCurrent,
+      layouts,
+      contentHeight,
+      headerHeight,
+      footerHeight,
+      cellData
+    } = this.state;
     return (
       <div
         style={{ ...scrollStyle, ...style, width, height }}
@@ -212,12 +305,37 @@ class RecyclerList<T> extends React.Component<RecyclerListProps<T>, State<T>> {
         onScroll={this.handleScroll}
         ref={this.container}
       >
-        {Header ? (
-          <Header onHeightChange={this.handleHeaderHeightChange} />
-        ) : null}
-        {Footer ? (
-          <Footer onHeightChange={this.handleFooterHeightChange} />
-        ) : null}
+        <div
+          style={{ width, height: headerHeight + footerHeight + contentHeight }}
+        >
+          {Header ? (
+            <Header
+              onHeightChange={this.handleHeaderHeightChange}
+              style={headerStyle}
+            />
+          ) : null}
+          {renderCurrent.map((layoutIndex, index) => {
+            const layout = layouts[layoutIndex];
+            const { type: TypeComponent, height, top } = layout;
+
+            return (
+              <TypeComponent
+                height={height}
+                width={width}
+                top={top + headerHeight}
+                index={layoutIndex}
+                data={cellData[layoutIndex].data}
+                key={index}
+              />
+            );
+          })}
+          {Footer ? (
+            <Footer
+              onHeightChange={this.handleFooterHeightChange}
+              style={footerStyle}
+            />
+          ) : null}
+        </div>
       </div>
     );
   }
