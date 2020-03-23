2f10e238ed7c037c48735d148896179bdd14dc9d
zhangw <935279748@qq.com>
2020-03-23 17:37:30 +0800

    update

--- parcel.js
+++ parcel.js
@@ -26,18 +26,11 @@ const options = {
   target: 'browser',
   bundleNodeModules: false,
   logLevel: 3,
-  /**
-   * 5 = 储存每个信息
-   * 4 = 输出信息、警告和错误附加时间戳和dev服务的http请求
-   * 3 = 输出信息、警告和错误
-   * 2 = 输出警告和错误
-   * 1 = 输出错误
-   */
-  hmr: true, // 开启或禁止HRM
-  hmrPort: 0, // hmr socket 运行的端口，默认为随机空闲端口(在 Node.js 中，0 会被解析为随机空闲端口)
-  sourceMaps: true, // 启用或禁用 sourcemaps，默认为启用(在精简版本中不支持)
-  hmrHostname: '', // 热模块重载的主机名，默认为 ''
-  detailedReport: false // 打印 bundles、资源、文件大小和使用时间的详细报告，默认为 false，只有在禁用监听状态时才打印报告
+  hmr: true,
+  hmrPort: 0,
+  sourceMaps: true,
+  hmrHostname: '',
+  detailedReport: false
 };
 
 (async function() {
